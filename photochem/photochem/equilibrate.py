41df893ab2044a452d584415f20f31081b3fbeb1
zmantas <46327508+zmantas@users.noreply.github.com>
2025-12-03 14:27:45 -0800

    Clean reset

--- photochem/photochem/equilibrate.py
+++
@@ -1,3 +0,0 @@
-from ._equilibrate import ChemEquiAnalysis, EquilibrateException
-from ._equilibrate import __version__
-from .utils import generate_zahnle_earth_thermo
--- photochem/photochem/extensions/__init__.py
+++
@@ -1,1 +0,0 @@
-from .gasgiants import EvoAtmosphereGasGiant
